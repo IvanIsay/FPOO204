4c514e1284ed629ae03a024b8a1dfa4cfc434953
lOkY <102552151+IvanIsay@users.noreply.github.com>
2025-01-22 14:42:22 -0600

    Estructura de un archivo java y Manejo de cadenas

--- Sintaxis/src/Main.java
+++ Sintaxis/src/Main.java
@@ -1,9 +1,31 @@
+
+//IMPORTACIONES
+
+import java.beans.JavaBean;
+
+//Clase Principal
 public class Main {
 
+    //Metodo Main
     public static void main(String[] args) {
 
+/*        comentariios
+                de varias lineas*/
+
+//        2. cadenas con Java
 
         System.out.println("Hello, Ivan Isay!");
+        String cadenas = "ivan" + "guerra" + "lopez";
+
+        System.out.println(cadenas);
+
+        System.out.println(cadenas.length());
+
+        System.out.println(cadenas.substring(2,5));
+        System.out.println(cadenas.substring(2));
+        System.out.println(cadenas.substring(0,5));
 
     }
+
+
 }
