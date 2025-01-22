c4f0559f1e5a2260916b5dcfa74555e6a4117cdd
lOkY <102552151+IvanIsay@users.noreply.github.com>
2025-01-22 15:35:39 -0600

    Finalizamos practica No2 Sintaxis

--- Sintaxis/src/Main.java
+++ Sintaxis/src/Main.java
@@ -1,7 +1,8 @@
 
 //IMPORTACIONES
+//import java.util.Random;
+import java.util.Scanner;
 
-import java.beans.JavaBean;
 
 //Clase Principal
 public class Main {
@@ -14,7 +15,7 @@ public class Main {
 
 //        2. cadenas con Java
 
-        System.out.println("Hello, Ivan Isay!");
+/*        System.out.println("Hello, Ivan Isay!");
         String cadenas = "ivan" + "guerra" + "lopez";
 
         System.out.println(cadenas);
@@ -23,7 +24,80 @@ public class Main {
 
         System.out.println(cadenas.substring(2,5));
         System.out.println(cadenas.substring(2));
-        System.out.println(cadenas.substring(0,5));
+        System.out.println(cadenas.substring(0,5));*/
+
+/*//        3. Variables
+
+        int x=5,x2;
+        double y=1.25,y2;
+        String z= "204",z1;
+
+        //Conversion de cadena a int
+        x2= Integer.parseInt(z);
+
+        //Conversion de double a cadena
+        z1= String.valueOf(y);
+
+        //Conversion de int a double
+        double asd= Double.valueOf(x);
+
+        //Conversion Implicita
+        int num= 12;
+        double numD= num;
+        System.out.println("Conversion Implicita"+numD);
+
+
+        System.out.println(x2);
+        System.out.println(z1);
+        System.out.println(asd);
+
+        //creamos un objeto rdn de clase Random
+        Random rdn= new Random();
+
+        //generamos numeros y los guardamos
+        int numAleatorio=  rdn.nextInt();
+        double numDouble= rdn.nextDouble();
+
+        System.out.println("Aleatorio entero: "+numAleatorio);
+        System.out.println("Aleatorio double: "+numDouble);*/
+
+      /*  //4. Solucitud de datos
+
+        Scanner scn=new Scanner(System.in);
+
+        System.out.println("Introduce cualquier dato");
+        String dato= scn.nextLine();
+
+        System.out.println("Introduce dato entero");
+        int datoentero= scn.nextInt();
+
+        System.out.println("Introduce dato con decimales");
+        double datodecimal= scn.nextDouble();
+
+        System.out.println("Cualquier dato"+ dato);
+        System.out.println("Dato entero"+ datoentero);
+        System.out.println("Dato decimal "+ datodecimal);*/
+
+
+//     5. Boolean, operadores logicos y de comparacion
+
+        System.out.println(10 > 9); //true
+        System.out.println(10 == 9); //false
+        System.out.println(10 < 9); //false
+        System.out.println(10 >= 9); //true
+        System.out.println(10 <= 9); // false
+        System.out.println(10 != 9); //true
+
+        int x= 3;
+        System.out.println(x< 5 && x>10); //false
+        System.out.println(x< 5 || x>10); //true
+        System.out.println(!(x< 5 && x>10)); //true
+
+
+
+
+
+
 
     }
 
